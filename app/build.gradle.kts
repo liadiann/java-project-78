13ce5fe70034cdc5b1c5cd210a054bdc08a92fae
Julia Kon <kon1976y@gmail.com>
2025-05-02 22:41:04 +0300

    add app

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    checkstyle
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "liadiann_java-project-78")
+        property("sonar.organization", "liadiann")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
